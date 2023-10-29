cb509c1dbfd2d61a6888cc41ba05614930893242
sssamy <sssamy@VALU-SSSAMY-NB>
2023-10-29 18:58:03 +0200

    push

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+
+Console.WriteLine("Hello, World!");
+
+
+//var api = OcrApi.Create();
+
+using (var api = OcrApi.Create())
+{
+    api.Init(Languages.Arabic);
+    //ScaleByPercent
+    string plainText = api.GetTextFromImage("C:\\Work\\Projects\\Testing Projects\\ConsoleApp1\\ConsoleApp1\\bin\\Debug\\net6.0\\images\\img4.jpeg");
+
+    Console.WriteLine(plainText);
+    Console.Read();
+}
